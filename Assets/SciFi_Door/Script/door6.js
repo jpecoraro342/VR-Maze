3a5eb3476aef02d3ff7d3e6b945f86b5e976ab37
cfkwok <cfkwok@ufl.edu>
2016-01-26 12:35:35 -0500

    Add doors

---
+++ Assets/SciFi_Door/Script/door6.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+
+
+function OnTriggerEnter (obj : Collider) {
+    var thedoor = gameObject.FindWithTag("SF_Door6");
+    thedoor.GetComponent.<Animation>().Play("open");
+}
+
+    function OnTriggerExit (obj : Collider) {
+        var thedoor = gameObject.FindWithTag("SF_Door6");
+        thedoor.GetComponent.<Animation>().Play("close");
+    }
