b0699510851d7155cb80d01e448ab751e2b288f4
cfkwok <cfkwok@ufl.edu>
2016-01-27 00:09:54 -0500

    Added ceiling and sounds

--- Assets/SciFi_Door/Script/door8.js
+++ Assets/SciFi_Door/Script/door8.js
@@ -1,12 +1,16 @@
 ﻿#pragma strict
-
+var opening:AudioClip;
 
 function OnTriggerEnter (obj : Collider) {
-    var thedoor = gameObject.FindWithTag("SF_Door8");
-    thedoor.GetComponent.<Animation>().Play("open");
+	var thedoor = gameObject.FindWithTag("SF_Door8");
+	thedoor.GetComponent.<Animation>().Play("open");
+	GetComponent.<AudioSource>().clip = opening;
+	GetComponent.<AudioSource>().Play();
+	yield WaitForSeconds (3);
+	GetComponent.<AudioSource>().Stop();
 }
 
-    function OnTriggerExit (obj : Collider) {
-        var thedoor = gameObject.FindWithTag("SF_Door8");
-        thedoor.GetComponent.<Animation>().Play("close");
-    }
+function OnTriggerExit (obj : Collider) {
+	var thedoor = gameObject.FindWithTag("SF_Door8");
+	thedoor.GetComponent.<Animation>().Play("close");
+}
