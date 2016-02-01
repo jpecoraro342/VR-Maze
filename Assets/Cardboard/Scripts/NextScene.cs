ca83da70ca32a40cc4224d1bb350c8c71a6567cd
cfkwok <cfkwok@ufl.edu>
2016-02-01 17:20:30 -0500

    Add screen fade

--- Assets/Cardboard/Scripts/NextScene.cs
+++ Assets/Cardboard/Scripts/NextScene.cs
@@ -3,13 +3,6 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class NextScene : MonoBehaviour {
-    void Start()
-    {
-    }
-
-    void Update()
-    {
-    }
     void OnCollisionEnter(Collision collision)
     {
         SceneManager.LoadScene("OutsideScene", LoadSceneMode.Single);
