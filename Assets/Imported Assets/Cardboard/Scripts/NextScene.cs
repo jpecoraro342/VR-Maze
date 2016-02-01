87510a5d6bb5e64103f147b2a2436c8455e611a4
CraigLu <Craig.Z.Lu@gmail.com>
2016-01-31 21:40:14 -0500

    adding google cardboard interaction

---
+++ Assets/Imported Assets/Cardboard/Scripts/NextScene.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour {
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        SceneManager.LoadScene("OutsideScene", LoadSceneMode.Single);
+
+    }
+}
