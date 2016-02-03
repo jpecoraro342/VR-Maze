8f2b2a1608aba60bc7e6b735cd27c14ec870e88e
jpecoraro342 <jpecoraro342@ufl.edu>
2016-02-03 02:53:01 -0500

    Basically finished my part, half done baking the lighting

---
+++ Assets/Scripts/Deathwing.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Deathwing : MonoBehaviour {
+
+	public AudioSource dragonSound;
+	public ParticleSystem flameParticles;
+
+	public float flameDuration;
+	public float particleSystemDelay;
+
+	private float timeWeHaveWeBeenFlaming;
+
+	private bool isFlaming;
+	private bool particlesStartedPlaying;
+
+	// Use this for initialization
+	void Start () {
+		stopAllFlaming();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isFlaming) {
+			timeWeHaveWeBeenFlaming += Time.deltaTime;
+
+			if (timeWeHaveWeBeenFlaming >= particleSystemDelay && !particlesStartedPlaying) {
+				startFlameParticleSystem();
+			}
+
+			if (timeWeHaveWeBeenFlaming >= flameDuration) {
+				stopAllFlaming();
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (!isFlaming) {
+			startFlaming();
+		}
+	}
+
+	void startFlaming() {
+		isFlaming = true;
+		timeWeHaveWeBeenFlaming = 0;
+		dragonSound.Play();
+	}
+
+	void startFlameParticleSystem() {
+		particlesStartedPlaying = true;
+		flameParticles.gameObject.SetActive(true);
+	}
+
+	void stopAllFlaming() {
+		isFlaming = false;
+		particlesStartedPlaying = false;
+		timeWeHaveWeBeenFlaming = 0;
+		dragonSound.Stop();
+		flameParticles.gameObject.SetActive(false);
+	}
+			
+}
---
+++ Assets/Scripts/Zombie.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zombie : MonoBehaviour {
+
+	Animator animator;
+
+	public AudioSource zombieSound;
+	public float lightDuration;
+	public GameObject lightSource;
+
+	bool isAttacking;
+	float currentAttackingTime;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+		resetValues();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isAttacking) {
+			currentAttackingTime += Time.deltaTime;
+
+			if (currentAttackingTime >= lightDuration) {
+				resetValues();
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		animator.SetTrigger("Attack");
+		isAttacking = true;
+		lightSource.SetActive(true);
+		zombieSound.Play();
+	}
+
+	void resetValues() {
+		lightSource.SetActive(false);
+		isAttacking = false;
+		currentAttackingTime = 0;
+		zombieSound.Stop();
+	}
+}
