44ef8985f68e4ac4cc8d25ce91945471dc89ff48
jpecoraro342 <jpecoraro342@ufl.edu>
2016-02-03 12:21:37 -0500

    Removed unused objects from dungeon scene to attempt to stop crashing

--- Assets/Scripts/NextScene.cs
+++ Assets/Scripts/NextScene.cs
@@ -6,11 +6,14 @@ public class NextScene : MonoBehaviour {
 
     public int sceneLevel;
 
+	int numberOfScenes;
+
     void OnCollisionEnter(Collision collision)
     {
         SceneManager.LoadScene("OutsideScene", LoadSceneMode.Single);
 
     }
+
     public void onClick()
     {
         SceneManager.LoadScene("OutsideScene", LoadSceneMode.Single);
